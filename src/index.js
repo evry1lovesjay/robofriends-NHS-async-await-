4d071946d23bdaed4686e104702dc9bdc8ba5bf6
evry1lovesjay <62482164+evry1lovesjay@users.noreply.github.com>
2020-12-24 01:59:26 +0100

    new for nhs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
